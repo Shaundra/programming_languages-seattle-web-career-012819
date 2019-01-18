10e270d8ce1096a11207b4b2fb85409fe733bfaf
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-01-18 15:44:19 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, langs|
+    langs.each do |language, type|
+      level_2 = type
+      level_2[:style] = [style]
+
+      if new_hash[language]
+        new_hash[language] = new_hash[language].merge(level_2) { |k, v1, v2| v1 == v2 ? v1 : v1 + v2 }
+      else
+        new_hash[language] = level_2
+      end
+    end
+  end
+
+  new_hash
+end
+
+def rl(languages)
+  new_hash = {}
+
+  languages.each do |style, langs|
+binding.pry
+      # level_2 = langs[:type]
+      # level_2[:style] = [style]
+      #
+      # if new_hash[language]
+      #   new_hash[language] = new_hash[language].merge(level_2) { |key, val1, val2| val1 == val2 ? val1 : val1 + val2 }
+      # else
+      #   new_hash[language] = level_2
+      # end
+  end
+  new_hash
 end
